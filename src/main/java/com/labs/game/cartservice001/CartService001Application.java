a364c3c60e7d08b64e8328409ca9d911a20649cd
tha220 <tha@dk.ibm.com>
2018-10-17 08:38:41 +0200

    initial commit

---
+++ src/main/java/com/labs/game/cartservice001/CartService001Application.java
@@ -0,0 +1,12 @@
+package com.labs.game.cartservice001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartService001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartService001Application.class, args);
+	}
+}
